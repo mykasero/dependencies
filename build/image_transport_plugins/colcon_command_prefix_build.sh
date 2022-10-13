d7e5b6ee127409f5eeae8c66f18eb72ee8de3165
Jacob <58263528+mykasero@users.noreply.github.com>
2022-10-13 13:55:33 +0200

    Add files via upload

---
+++ build/image_transport_plugins/colcon_command_prefix_build.sh
@@ -0,0 +1,6 @@
+# generated from colcon_core/shell/template/command_prefix.sh.em
+. "/home/ubuntu/test_ros_ws/install/cv_bridge/share/cv_bridge/package.sh"
+. "/home/ubuntu/test_ros_ws/install/image_transport/share/image_transport/package.sh"
+. "/home/ubuntu/test_ros_ws/install/compressed_depth_image_transport/share/compressed_depth_image_transport/package.sh"
+. "/home/ubuntu/test_ros_ws/install/compressed_image_transport/share/compressed_image_transport/package.sh"
+. "/home/ubuntu/test_ros_ws/install/theora_image_transport/share/theora_image_transport/package.sh"
