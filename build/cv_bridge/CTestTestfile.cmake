dd56f8f15b92119069b3d3376d3704fa91aff09b
Jacob <58263528+mykasero@users.noreply.github.com>
2022-10-13 13:39:14 +0200

    Add files via upload

---
+++ build/cv_bridge/CTestTestfile.cmake
@@ -0,0 +1,9 @@
+# CMake generated Testfile for 
+# Source directory: /home/ubuntu/test_ros_ws/src/vision_opencv/cv_bridge
+# Build directory: /home/ubuntu/test_ros_ws/build/cv_bridge
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("python")
+subdirs("src")
+subdirs("test")
